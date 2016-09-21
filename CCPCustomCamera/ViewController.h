3afc48fc989dbe0d6855969e2608fb544497bcd6
CCP <335966179@qq.com>
2016-09-21 17:23:30 +0800

    git init

---
+++ CCPCustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPCustomCamera
+//
+//  Created by DR on 16/9/21.
+//  Copyright © 2016年 CCP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
