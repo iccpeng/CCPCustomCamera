bb33dd60c9373a2b62c5f8d201b9b7017d956211
CCP <335966179@qq.com>
2017-03-07 14:56:44 +0800

    GPUImage的简单使用

--- CCPCustomCamera/CCPCustomCamera/Beautiful/CCPBecomeBeautifulController.m
+++ CCPCustomCamera/CCPCustomCamera/Beautiful/CCPBecomeBeautifulController.m
@@ -13,56 +13,125 @@
 @interface CCPBecomeBeautifulController ()
 @property (weak, nonatomic) IBOutlet UIImageView *becomeImageView;
 @property (weak, nonatomic) IBOutlet UIScrollView *becomeScrollview;
+@property (nonatomic,strong) GPUImagePicture *imagePicture;
+@property (nonatomic,strong) NSMutableArray *filterNameArray;
+@property (nonatomic,strong) NSMutableArray *filterStyleArray;
 
 @end
 
 @implementation CCPBecomeBeautifulController
 
+- (NSMutableArray *)filterNameArray {
+    
+    if (_filterNameArray == nil) {
+        
+        _filterNameArray = [NSMutableArray arrayWithObjects:@"原图",@"美白",@"高亮",@"怀旧",@"模糊", nil];
+    }
+    return _filterNameArray;
+}
+
+- (NSMutableArray *)filterStyleArray {
+    
+    if (_filterStyleArray == nil) {
+        GPUImageSepiaFilter *passthroughFilter = [[GPUImageSepiaFilter alloc] init];
+        _filterStyleArray = [NSMutableArray arrayWithObjects:@"",passthroughFilter,passthroughFilter,passthroughFilter,passthroughFilter, nil];
+    }
+    return _filterStyleArray;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self makeUI];
-    //设置滤镜的效果
-    GPUImageBeautifyFilter *passthroughFilter = [[GPUImageBeautifyFilter alloc] init];
-    //设置要渲染的区域
-    [passthroughFilter forceProcessingAtSize:self.becomeImageView.image.size];
-    [passthroughFilter useNextFrameForImageCapture];
-    //获取数据源
-    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:self.becomeImageView.image];
-    //加上滤镜
-    [stillImageSource addTarget:passthroughFilter];
-    //开始渲染
-    [stillImageSource processImage];
-    //获取渲染后的图片
-    UIImage *newImage = [passthroughFilter imageFromCurrentFramebuffer];
-    self.becomeImageView.image = newImage;
-    [[GPUImageContext sharedImageProcessingContext].framebufferCache purgeAllUnassignedFramebuffers];
 }
 
 - (void)makeUI {
     self.becomeScrollview.contentSize = CGSizeMake(530, 0);
-    self.becomeScrollview.showsVerticalScrollIndicator = YES;
-    self.becomeScrollview.backgroundColor = [UIColor redColor];
+    self.becomeScrollview.showsHorizontalScrollIndicator = NO;
+    UIImage *image = [UIImage imageNamed:@"tongliya"];
     for (int i = 0; i < 5; i++) {
+        UIImage *newImage = [self becomeBeautiful:i image:image];
         UIButton *becomeButton = [[UIButton alloc] init];
         becomeButton.frame = CGRectMake(5 + 105 * i, 0, 100, 100);
-        [becomeButton setTitle:[NSString stringWithFormat:@"%d",i] forState:UIControlStateNormal];
-        [becomeButton setBackgroundColor:[UIColor orangeColor]];
+        [becomeButton setTitle:[NSString stringWithFormat:@"%@",self.filterNameArray[i]] forState:UIControlStateNormal];
+        becomeButton.titleEdgeInsets = UIEdgeInsetsMake(0, 0, -70, 0);
+        [becomeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [becomeButton setBackgroundImage:newImage forState:UIControlStateNormal];
         becomeButton.tag = 100 + i;
         [becomeButton addTarget:self action:@selector(clickTheBtn:) forControlEvents:UIControlEventTouchUpInside];
         [self.becomeScrollview addSubview:becomeButton];
-        
     }
-    
 }
 
 - (void)clickTheBtn:(UIButton *)btn {
-    
-    NSLog(@"%@--%ld",btn,btn.tag);
+    UIImage *imageTest = [UIImage imageNamed:@"tongliya"];
+    UIImage *newImage = [self becomeBeautiful:btn.tag - 100 image:imageTest];
+    self.becomeImageView.image = newImage;
 }
 
-- (void) becomeBeautiful {
+- (UIImage *) becomeBeautiful:(NSInteger)tag image:(UIImage *)image {
     
-    
+    switch (tag) {
+        case 0:
+            return image;
+        case 1:{
+            //美颜
+            GPUImageBeautifyFilter *filter = [[GPUImageBeautifyFilter alloc] init];
+            [filter forceProcessingAtSize:image.size];
+            [filter useNextFrameForImageCapture];
+            self.imagePicture = [[GPUImagePicture alloc] initWithImage:image];
+            [self.imagePicture addTarget:filter];
+            [self.imagePicture processImage];
+            UIImage *newImage = [filter imageFromCurrentFramebuffer];
+            return newImage;
+        }
+            break;
+        case 2:{
+            //高亮
+            GPUImageBrightnessFilter *filter = [[GPUImageBrightnessFilter alloc] init];
+            filter.brightness = 0.3;
+            //设置要渲染的区域
+            [filter forceProcessingAtSize:image.size];
+            //捕获图片效果
+            [filter useNextFrameForImageCapture];
+            self.imagePicture = [[GPUImagePicture alloc] initWithImage:image];
+            //加上滤镜
+            [self.imagePicture addTarget:filter];
+            //开始渲染
+            [self.imagePicture processImage];
+            //获取渲染后的图片
+            UIImage *newImage = [filter imageFromCurrentFramebuffer];
+            return newImage;
+            
+        }
+            break;
+        case 3:{
+            //怀旧
+            GPUImageSepiaFilter *filter = [[GPUImageSepiaFilter alloc] init];
+            [filter forceProcessingAtSize:image.size];
+            [filter useNextFrameForImageCapture];
+            self.imagePicture = [[GPUImagePicture alloc] initWithImage:image];
+            [self.imagePicture addTarget:filter];
+            [self.imagePicture processImage];
+            UIImage *newImage = [filter imageFromCurrentFramebuffer];
+            return newImage;
+         }
+            break;
+        case 4:{
+            //高斯模糊
+            GPUImageGaussianBlurFilter *filter = [[GPUImageGaussianBlurFilter alloc] init];
+            [filter forceProcessingAtSize:image.size];
+            [filter useNextFrameForImageCapture];
+            self.imagePicture = [[GPUImagePicture alloc] initWithImage:image];
+            [self.imagePicture addTarget:filter];
+            [self.imagePicture processImage];
+            UIImage *newImage = [filter imageFromCurrentFramebuffer];
+            return newImage;
+        }
+            break;
+        default:
+            break;
+    }
+    return image;
 }
 
 
