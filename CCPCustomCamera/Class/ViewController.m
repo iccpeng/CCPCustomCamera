29232498b43f0fca19b56a9af5441ef89405e86b
CCP <335966179@qq.com>
2017-03-13 09:38:04 +0800

    GPUImage的简单使用

--- CCPCustomCamera/Class/ViewController.m
+++ CCPCustomCamera/Class/ViewController.m
@@ -30,7 +30,6 @@
     self.iconImageView.alpha = 0.9f;
     //设置字体样式
     self.englishNameLabel.font = [UIFont fontWithName:@"Zapfino" size:16];
-    
     //UIKIT_EXTERN const CGFloat UIFontWeightThin NS_AVAILABLE_IOS(8_2); 字体变细
     self.appNameLabel.font = [UIFont systemFontOfSize:22 weight:UIFontWeightThin];
 }
